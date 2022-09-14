4b502c9caf0f14844af408a3d6562352ec75893f
jirivls <voleskyjirka@gmail.com>
2022-09-14 17:21:47 +0200

    final backup from wednesday, one more task done

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -557,6 +557,34 @@ namespace GreenFox
             {
                 Console.WriteLine(i);
             }
+
+            //task25
+            // Create a program that asks for a number and prints the multiplication table with that number
+            //
+            // Example:
+            // The number 15 should print:
+            //
+            // 1 * 15 = 15
+            // 2 * 15 = 30
+            // 3 * 15 = 45
+            // 4 * 15 = 60
+            // 5 * 15 = 75
+            // 6 * 15 = 90
+            // 7 * 15 = 105
+            // 8 * 15 = 120
+            // 9 * 15 = 135
+            // 10 * 15 = 150
+
+            int inputMultiplier;
+            int maxMulti = 10;
+
+            Console.WriteLine("Please provide input multiplier.");
+            inputMultiplier = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + " * " + inputMultiplier + " = " + i*inputMultiplier);
+            }
         }
     }
 }
